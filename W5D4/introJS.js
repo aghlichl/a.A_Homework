0c934dc39bc8b57fd1a0936815375a26c6ed9794
Ari Ghlichloo <aurashghlichloo@gmail.com>
2019-02-06 23:30:10 -0800

    Add Hmwk W5D4

---
+++ W5D4/introJS.js
@@ -0,0 +1,50 @@
+function mysteryScoping1() {
+    var x = 'out of block';
+    if (true) {
+        var x = 'in block';
+        console.log(x);
+    }
+    console.log(x);
+}
+
+function mysteryScoping2() {
+    const x = 'out of block';
+    if (true) {
+        const x = 'in block';
+        console.log(x);
+    }
+    console.log(x);
+}
+
+function mysteryScoping3() {
+    const x = 'out of block';
+    if (true) {
+        var x = 'in block';
+        console.log(x);
+    }
+    console.log(x);
+}
+
+function mysteryScoping4() {
+    let x = 'out of block';
+    if (true) {
+        let x = 'in block';
+        console.log(x);
+    }
+    console.log(x);
+}
+
+function mysteryScoping5() {
+    let x = 'out of block';
+    if (true) {
+        let x = 'in block';
+        console.log(x);
+    }
+    let x = 'out of block again';
+    console.log(x);
+}
+
+function madLib(noun, verb, adj) {
+    console.log('we shall ' + verb + ' the ' + adj + ' ' + noun)
+}
+
